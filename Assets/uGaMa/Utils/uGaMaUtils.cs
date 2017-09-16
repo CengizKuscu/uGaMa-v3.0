c02d4b155bedf5599eb7099909e8806a037e02ba
CengizKuscu[Home] <cengizkuscu@gmail.com>
2017-09-16 13:05:37 +0300

    update: Utils

--- Assets/uGaMa/Utils/uGaMaUtils.cs
+++ Assets/uGaMa/Utils/uGaMaUtils.cs
@@ -5,29 +5,6 @@ namespace uGaMa.Utils
 {
     public static class uGaMaUtils
     {
-//        public static string EncryptDecrypt(string str)
-//        {
-//            StringBuilder inSb = new StringBuilder(str);
-//            StringBuilder outSb = new StringBuilder(str.Length);
-//
-//            char c;
-//            for (int i = 0; i < str.Length; i++)
-//            {
-//                c = inSb[i];
-//                c = (char)(c ^ 129);
-//                outSb.Append(c);
-//            }
-//
-//            return outSb.ToString();
-//        }
-
-        public static byte[] GetByteString (string str)
-        {
-            byte[] byteStr = Encoding.UTF8.GetBytes(str);
-
-            return byteStr;
-        }
-
         /// <summary>
         /// Gets or add a component. Usage example:
         /// BoxCollider boxCollider = transform.GetOrAddComponent<BoxCollider>(gameobject);
@@ -47,7 +24,7 @@ namespace uGaMa.Utils
         /// Remove a component. Usage example:
         /// transform.RemoveComponent<BoxCollider>(gameobject);
         /// </summary>
-        internal static void RemoveComponent<T>(GameObject gameObject) where T : Component
+        public static void RemoveComponent<T>(GameObject gameObject) where T : Component
         {
             T result = gameObject.GetComponent<T>();
             if(result != null)
